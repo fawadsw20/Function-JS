7b49033bac0d1d24a787cb4bd5d3b1e3b94442d1
fawadsw20 <fawadhassan.sw1997@gmail.com>
2024-07-11 12:25:26 +0500

    Add files via upload

---
+++ Functions JS/script.js
@@ -0,0 +1,40 @@
+//How to declare function in different ways.. 
+
+function SayMyName (){
+
+    console.log("F");
+    console.log("A");
+    console.log("W");
+    console.log("A");
+    console.log("D");
+
+}
+
+SayMyName();
+
+function BasicOperation (number1, number2){
+
+    console.log(number1+number2);
+}
+
+BasicOperation(2,5);
+
+function additionOperation(number1, number2){
+
+    return number1+number2;
+
+}
+
+console.log(additionOperation(4,6));
+
+function subOperation (number1, number2){
+
+    let sub = number1-number2;
+
+    return sub;
+}
+
+console.log(subOperation(8,4))
+
+
+
